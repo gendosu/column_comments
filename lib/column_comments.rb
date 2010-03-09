42a86f3fc85d812e50065547712d6153321e38df
root <gen_takahashi@yahoo.co.jp>
2010-03-09 21:39:52 +0900

    Rails 2.3上でDecimal型のカラムを扱う時に不具合が出ていたので修正 根本的な原因は、SchemaDumperの中のtableメソッドをオーバーライドしてるが、その中身が古いため

--- lib/column_comments.rb
+++ lib/column_comments.rb
@@ -103,11 +103,13 @@ module ActiveRecord
         begin
           tbl = StringIO.new
 
+          # first dump primary key column
           if @connection.respond_to?(:pk_and_sequence_for)
             pk, pk_seq = @connection.pk_and_sequence_for(table)
+          elsif @connection.respond_to?(:primary_key)
+            pk = @connection.primary_key(table)
           end
-          pk ||= 'id'
-
+          
           tbl.print "  create_table #{table.inspect}"
           if columns.detect { |c| c.name == pk }
             if pk != 'id'
@@ -119,20 +121,50 @@ module ActiveRecord
           tbl.print ", :force => true"
           tbl.puts " do |t|"
 
-          columns.each do |column|
+          # then dump all non-primary key columns
+          column_specs = columns.map do |column|
             raise StandardError, "Unknown type '#{column.sql_type}' for column '#{column.name}'" if @types[column.type].nil?
             next if column.name == pk
-            tbl.print "    t.column #{column.name.inspect}, #{column.type.inspect}"
-            tbl.print ", :limit => #{column.limit.inspect}" if column.limit != @types[column.type][:limit] 
-            tbl.print ", :default => #{column.default.inspect}" if !column.default.nil?
-            tbl.print ", :null => false" if !column.null
-            tbl.print ", :comment => #{column.comment.inspect}" if !column.comment.nil?
+            spec = {}
+            spec[:name]      = column.name.inspect
+            spec[:type]      = column.type.to_s
+            spec[:limit]     = column.limit.inspect if column.limit != @types[column.type][:limit] && column.type != :decimal
+            spec[:precision] = column.precision.inspect if !column.precision.nil?
+            spec[:scale]     = column.scale.inspect if !column.scale.nil?
+            spec[:null]      = 'false' if !column.null
+            spec[:default]   = default_string(column.default) if column.has_default?
+            spec[:comment]   = column.comment.inspect if !column.comment.nil?
+            (spec.keys - [:name, :type]).each{ |k| spec[k].insert(0, "#{k.inspect} => ")}
+            spec
+          end.compact
+
+          # find all migration keys used in this table
+          keys = [:name, :limit, :precision, :scale, :default, :null, :comment] & column_specs.map(&:keys).flatten
+
+          # figure out the lengths for each column based on above keys
+          lengths = keys.map{ |key| column_specs.map{ |spec| spec[key] ? spec[key].length + 2 : 0 }.max }
+
+          # the string we're going to sprintf our values against, with standardized column widths
+          format_string = lengths.map{ |len| "%-#{len}s" }
+
+          # find the max length for the 'type' column, which is special
+          type_length = column_specs.map{ |column| column[:type].length }.max
+
+          # add column type definition to our format string
+          format_string.unshift "    t.%-#{type_length}s "
+
+          format_string *= ''
+
+          column_specs.each do |colspec|
+            values = keys.zip(lengths).map{ |key, len| colspec.key?(key) ? colspec[key] + ", " : " " * len }
+            values.unshift colspec[:type]
+            tbl.print((format_string % values).gsub(/,\s*$/, ''))
             tbl.puts
           end
 
           tbl.puts "  end"
           tbl.puts
-        
+          
           indexes(table, tbl)
 
           tbl.rewind
@@ -142,7 +174,7 @@ module ActiveRecord
           stream.puts "#   #{e.message}"
           stream.puts
         end
-      
+        
         stream
       end
   end
